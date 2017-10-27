1cb56ff870ca352d14668f26e8a687247e53748f
syedraza04 <syedraza04@gmail.com>
2017-10-27 15:13:59 -0400

    dynamic styling added along with dynamic classes

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
 
   render() {
     const style={
-        backgroundColor:'white',
+        backgroundColor:'green',
         font:'inherit',
         border:'1px solid blue',
         padding:'8px',
@@ -67,10 +67,22 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
     }
+      // let classes=['red','bold'].join(' '); // "red bold"
+    const classes=[]; // "red bold"
+
+    if (this.state.persons.length <=2){
+        classes.push('red');
+    }
+    if (this.state.persons.length <=1){
+        classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working</p>
         <button
            style={style}
            onClick={this.togglePersonHandler}>Toggle Person</button>
